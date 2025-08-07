e42846cd187e1044346c86146d67852af41daff0
Marcwos <ubituna22@gmail.com>
2025-08-06 21:54:37 -0500

    refactor: Remove

--- backend/core/db.py
+++
@@ -1,9 +0,0 @@
-# core/db.py
-from motor.motor_asyncio import AsyncIOMotorClient
-from core.config import settings
-
-client = AsyncIOMotorClient(settings.database.mongodb_uri)
-db = client[settings.database.mongo_db_name]
-
-def get_db():
-    return db
--- backend/core/envs/key.py
+++
@@ -1,3 +0,0 @@
-import secrets
-
-print(secrets.token_hex(32)) # This will generate a random 64-character hexadecimal string
